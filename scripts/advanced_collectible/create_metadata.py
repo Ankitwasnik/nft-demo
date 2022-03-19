f62cc1ec6bbbc2951128b5c8be4248e17773115d
ankitwasnik <ankit@emtech.com>
2022-03-19 15:23:12 +0530

    Upload to ipfs and pinata

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -3,6 +3,8 @@ from brownie import AdvancedCollectible, network
 from scripts.helpful_scripts import get_breed
 from metadata.sample_metadata import metadata_template
 from pathlib import Path
+import requests
+import json
 
 
 def main():
@@ -14,11 +16,32 @@ def main():
         metadata_filename = f"./metadata/{network.show_active()}/{token_id}-{breed}.json"
         print(metadata_filename)
 
-    collectible_metadata = metadata_template
-    if Path(metadata_filename).exists():
-        print(f"{metadata_filename} already exists. Delete it to override")
-    else:
-        print(f"Creating metdata file: {metadata_filename}")
-        collectible_metadata["name"] = breed
-        collectible_metadata["description"] = f"An adorable {breed} pup!"
-        print(collectible_metadata)
+        collectible_metadata = metadata_template
+        if Path(metadata_filename).exists():
+            print(f"{metadata_filename} already exists. Delete it to override")
+        else:
+            print(f"Creating metdata file: {metadata_filename}")
+            collectible_metadata["name"] = breed
+            collectible_metadata["description"] = f"An adorable {breed} pup!"
+            image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
+            image_uri = upload_to_ipfs(image_path)
+            collectible_metadata["image"] = image_uri
+            print(collectible_metadata)
+            with open(metadata_filename, "w") as file:
+                json.dump(collectible_metadata, file)
+            upload_to_ipfs(metadata_filename)    
+
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        ipfs_url = "http://127.0.0.1:5001"
+        endpoint = "/api/v0/add"
+        response = requests.post(ipfs_url+endpoint, files={"file":image_binary})    
+        ipfs_hash = response.json()["Hash"]
+        # "./img/PUG.png" --> PUG.png
+        filename = filepath.split("/")[-1:][0]
+        image_url = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+        # "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+        print(image_url)
+        return image_url
